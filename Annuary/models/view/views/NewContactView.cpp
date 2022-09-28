0733d84f3c98018d1adee4d533bee96cae40b73b
Alexis Haldy <alexishaldy@gmail.com>
2022-09-28 10:15:03 +0200

    Add comments to new contact view

--- Annuary/models/view/views/NewContactView.cpp
+++ Annuary/models/view/views/NewContactView.cpp
@@ -4,20 +4,26 @@
 
 void NewContactView::Update(Controller* controller, Screen& screen)
 {
+	// Draw name field
 	screen.Draw(Text{ .str = "Name:", .x = screen.GetWidth() / 4, .y = 5 });
 	screen.Draw(Field{ .text = _name, .x = screen.GetWidth() / 4 + 6 + 3, .y = 5, .selected = _currentButton == 0 });
+	// Draw phone number field
 	screen.Draw(Text{ .str = "Phone number:", .x = screen.GetWidth() / 4, .y = 7 });
 	screen.Draw(Field{ .text = _number, .x = screen.GetWidth() / 4 + 13 + 3, .y = 7, .selected = _currentButton == 1 });
+	// Draw save button
 	screen.Draw(Button{ .text = "Save", .x = screen.GetWidth() / 2, .y = 9, .selected = _currentButton == 2, .xCentered = true });
+	// Draw the error message if there is any
 	screen.Draw(Text{ .str = _errorMessage, .x = screen.GetWidth() / 2, .y = screen.GetHeight() / 2, .xCentered = true, .background = Background::RED, .foreground = Foreground::WHITE });
-
+	// Display controls for the user
 	screen.Draw(Text{ .str = "Back: Esc | Arrows: move | Confirm: Enter", .x = screen.GetWidth() / 2, .y = screen.GetHeight() - 3, .xCentered = true });
 }
 
 void NewContactView::OnKeyPressed(Controller* controller, const char key)
 {
+	// Clear the error message every time the user press a key
 	_errorMessage.clear();
 
+	// Change the selected field/button with up/down arrows
 	if (key == KEY_UP)
 	{
 		_currentButton--;
@@ -28,11 +34,14 @@ void NewContactView::OnKeyPressed(Controller* controller, const char key)
 	}
 	else if (key == KEY_ENTER)
 	{
+		// Save the new contact if on save, otherwise switch to next button
 		if (_currentButton == _maxButtons - 1)
 		{
+			// Trim fields
 			_name = StringUtility::trim(_name);
 			_number = StringUtility::trim(_number);
 
+			// If one of the fields are empty, display an error message
 			if (_name.empty())
 			{
 				_errorMessage = "The name is empty !";
@@ -43,6 +52,7 @@ void NewContactView::OnKeyPressed(Controller* controller, const char key)
 			}
 			else
 			{
+				// Add the new contact to the contacts
 				const auto mainController = dynamic_cast<MainController*>(controller);
 				mainController->AddContact(_name, _number);
 				mainController->GoBack();
@@ -55,6 +65,7 @@ void NewContactView::OnKeyPressed(Controller* controller, const char key)
 	}
 	else if (key == KEY_BACKSPACE)
 	{
+		// Remove a character from the selected field if he's not empty
 		if (_currentButton == 0)
 		{
 			if (!_name.empty())
@@ -74,8 +85,10 @@ void NewContactView::OnKeyPressed(Controller* controller, const char key)
 	{
 		if (_currentButton == 0)
 		{
+			// Limit the characters used to be a-z, A-Z or space
 			if (key <= 90 && key >= 65 || key <= 122 && key >= 97 || key == 32)
 			{
+				// Limit the length to 60 characters maximum
 				if (_name.length() < 60)
 				{
 					_name += key;
@@ -87,8 +100,10 @@ void NewContactView::OnKeyPressed(Controller* controller, const char key)
 		}
 		else if (_currentButton == 1)
 		{
+			// Limit the characters used to be 0-9, + or space
 			if (key >= 48 && key <= 57 || key == 43 || key == 32)
 			{
+				// Limit the length to 40 characters maximum
 				if (_number.length() < 40)
 				{
 					_number += key;
