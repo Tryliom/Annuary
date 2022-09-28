7548cbd3d3a47bb6c4af12047d098ad66496bf59
Alexis Haldy <alexishaldy@gmail.com>
2022-09-28 10:43:20 +0200

    Add comments to search view

--- Annuary/models/view/views/SearchView.cpp
+++ Annuary/models/view/views/SearchView.cpp
@@ -5,11 +5,14 @@
 
 void SearchView::Update(Controller* controller, Screen& screen)
 {
+	// Draw the title of the view
 	screen.Draw(Text{ .str = "Search a contact", .x = screen.GetWidth() / 2, .y = 2, .xCentered = true });
+	// Draw the name field
 	screen.Draw(Text{ .str = "Name:", .x = screen.GetWidth() / 4, .y = 5 });
 	screen.Draw(Field{ .text = _name, .x = screen.GetWidth() / 4 + 6 + 3, .y = 5, .selected = true });
+	// Display the search results
 	screen.Draw(Text{ .str = _message, .x = screen.GetWidth() / 2, .y = 7, .xCentered = true });
-
+	// Display controls for the user
 	screen.Draw(Text{ .str = "Back: Esc", .x = screen.GetWidth() / 2, .y = screen.GetHeight() - 3, .xCentered = true });
 }
 
@@ -24,11 +27,13 @@ void SearchView::OnKeyPressed(Controller* controller, const char key)
 			_name.pop_back();
 		}
 	}
-	else if (key < 127 && key > 19)
+	// Limit the characters used to be a-z, A-Z or space
+	else if (key <= 90 && key >= 65 || key <= 122 && key >= 97 || key == 32)
 	{
 		_name += key;
 	}
 
+	// Update message to display the search results
 	if (contacts.contains(_name))
 	{
 		_message = "Number found: " + contacts[_name];
